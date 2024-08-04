e3855f8b11bab376d1bb28de2d265c6e36c9206a
MuhammadSaad0 <muhammadsaadz100@gmail.com>
2024-08-05 00:06:09 +0500

    feat: round robin upstream selection

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
@@ -31,23 +32,55 @@ import (
 var configDb *sql.DB
 var rwLock = sync.RWMutex{}
 
-func ReverseProxy(writer http.ResponseWriter, request *http.Request) { 
+func ReverseProxy(writer http.ResponseWriter, request *http.Request) {
 	// copy incoming request
 	modifiedRequest := request.Clone(context.Background())
 	path := request.URL.Path
 	query := request.URL.Query()
-	rows, err := configDb.Query("SELECT URL FROM UPSTREAMS")
+
+	var rows *sql.Rows
+	var err error
+
+	rows, err = configDb.Query("SELECT SETTING_VALUE FROM SETTINGS WHERE SETTING_NAME='LOAD_BALANCING_STRATEGY';")
 	if err != nil {
 		fmt.Fprintln(writer, err.Error())
 		return
 	}
-	rows.Next() // use the first upstream url for now
+
+	rows.Next()
+	var loadBalancingStrat int
+	rows.Scan(&loadBalancingStrat)
+
+	rows, err = configDb.Query("SELECT URL FROM UPSTREAMS")
+	if err != nil {
+		fmt.Fprintln(writer, err.Error())
+		return
+	}
 	var data string
-	err = rows.Scan(&data)
-	if err != nil {
-		fmt.Fprintln(writer, err.Error())
-		return
+	if loadBalancingStrat == 0 { // just use first url
+		rows.Next()
+		err = rows.Scan(&data)
+		if err != nil {
+			fmt.Fprintln(writer, err.Error())
+			return
+		}
+
+	} else if loadBalancingStrat == 1 { // round robin
+		upstreams := make([]string, 0, 10)
+
+		for rows.Next() {
+			err = rows.Scan(&data)
+			if err != nil {
+				fmt.Fprintln(writer, err.Error())
+				return
+			}
+			upstreams = append(upstreams, data)
+		}
+
+		roundRobinNum := rand.Intn(len(upstreams))
+		data = upstreams[roundRobinNum]
 	}
+
 	url, err := url.Parse(data)
 	if err != nil {
 		fmt.Fprintln(writer, "Error Parsing url")
@@ -60,24 +93,23 @@ func ReverseProxy(writer http.ResponseWriter, request *http.Request) {
 
 	defaultClient := http.Client{
 		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-			},
-		},	
+			TLSClientConfig: &tls.Config{},
+		},
 	}
 	response, err := defaultClient.Do(modifiedRequest) // perform http request
 	if err != nil {
 		fmt.Fprintf(writer, "Error sending request %s", err.Error())
 		return
 	}
-	defer response.Body.Close() // close body to make sure socket isnt left hanging
+	defer response.Body.Close()                // close body to make sure socket isnt left hanging
 	for key, values := range response.Header { // write response headers to writer
 		for _, value := range values {
 			writer.Header().Add(key, value)
 		}
 	}
 	writer.WriteHeader(response.StatusCode) // write response status code to writer
-	
-	io.Copy(writer, response.Body) // copy response body to writer 
+
+	io.Copy(writer, response.Body) // copy response body to writer
 
 }
 
@@ -92,16 +124,16 @@ func ReturnUpstreams(wrriter http.ResponseWriter, request *http.Request) {
 		return
 	}
 	rwLock.RLock() // acquire read lock to make sure if write lock has been acquired this read is blocked
-	for rows.Next(){
-		var upstreamId interface {}
-		var upstreamUrl interface {}
+	for rows.Next() {
+		var upstreamId interface{}
+		var upstreamUrl interface{}
 		err = rows.Scan(&upstreamId, &upstreamUrl)
 		if err != nil {
 			rwLock.RUnlock()
 			fmt.Fprintln(wrriter, err.Error())
 			return
 		}
-		var finalData map[string] interface {} = make(map[string]interface{})
+		var finalData map[string]interface{} = make(map[string]interface{})
 		finalData["id"] = upstreamId
 		finalData["url"] = upstreamUrl
 		toRet = append(toRet, finalData)
@@ -110,7 +142,7 @@ func ReturnUpstreams(wrriter http.ResponseWriter, request *http.Request) {
 	response := make(map[string][]interface{})
 	if len(toRet) > 0 {
 		response["proxies"] = toRet
-	}else{
+	} else {
 		response["proxies"] = make([]interface{}, 0)
 	}
 	encoder := json.NewEncoder(wrriter)
@@ -121,7 +153,7 @@ type addUpstream struct {
 	Url string `json:"url"`
 }
 
-func AddUpstream(writer http.ResponseWriter, request *http.Request){
+func AddUpstream(writer http.ResponseWriter, request *http.Request) {
 	decoder := json.NewDecoder(request.Body)
 	defer request.Body.Close()
 	var data addUpstream
@@ -157,7 +189,9 @@ func DeleteUpstream(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
+	rwLock.Lock()
 	_, err = configDb.Query("DELETE FROM UPSTREAMS WHERE ID = ?", data.Id)
+	rwLock.Unlock()
 
 	if err != nil {
 		fmt.Fprintln(writer, err.Error())
@@ -168,14 +202,48 @@ func DeleteUpstream(writer http.ResponseWriter, request *http.Request) {
 
 }
 
+type updateLoadBalancing struct {
+	Strategy int `json:"strategy"`
+}
+
+// 0 -> use first upstream url
+// 1 -> round robin upstream selection
+func UpdateLoadBalancingStrat(writer http.ResponseWriter, request *http.Request) {
+	decoder := json.NewDecoder(request.Body)
+	defer request.Body.Close()
+	var data updateLoadBalancing
+	var err error
+	err = decoder.Decode(&data)
+	if err != nil {
+		fmt.Fprintln(writer, err.Error())
+		return
+	}
+
+	rwLock.Lock()
+	_, err = configDb.Query("UPDATE SETTINGS SET SETTING_VALUE=? WHERE SETTING_NAME='LOAD_BALANCING_STRATEGY'", data.Strategy)
+	rwLock.Unlock()
+	if err != nil {
+		fmt.Fprintln(writer, err.Error())
+		return
+	}
+	fmt.Fprintln(writer, "Load balancing strategy updated")
+}
+
 func main() {
 	var err error
 	configDb, err = sql.Open("sqlite", "config.db")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 	}
-	configStatement := "CREATE TABLE IF NOT EXISTS UPSTREAMS (ID INTEGER PRIMARY KEY, URL STRING);" // initialize config table
-	_, err = configDb.Exec(configStatement)
+	upstreamsStatement := "CREATE TABLE IF NOT EXISTS UPSTREAMS (ID INTEGER PRIMARY KEY, URL STRING);"                                                                                                       // initialize config table
+	settingsStatement := "CREATE TABLE IF NOT EXISTS SETTINGS (ID INTEGER PRIMARY KEY, SETTING_NAME STRING, SETTING_VALUE INT); INSERT OR IGNORE INTO SETTINGS VALUES (NULL, 'LOAD_BALANCING_STRATEGY', 0);" // initialize settings table
+
+	_, err = configDb.Exec(upstreamsStatement)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+	}
+
+	_, err = configDb.Exec(settingsStatement)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 	}
@@ -183,6 +251,7 @@ func main() {
 	http.HandleFunc("GET /config/upstreams", ReturnUpstreams)
 	http.HandleFunc("POST /config/add-upstream", AddUpstream)
 	http.HandleFunc("DELETE /config/delete-upstream", DeleteUpstream)
+	http.HandleFunc("PATCH /config/update-load-balancing-strategy", UpdateLoadBalancingStrat)
 	// http.Handle("GET /", http.FileServer(http.Dir("./")))
 	http.HandleFunc("/proxy/*", ReverseProxy)
 	log.Fatal(http.ListenAndServe(":3690", nil))
