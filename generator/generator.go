6b273192dea5c9a5fea9e560f37de16b5d7472e5
MuhammadSaad0 <muhammadsaadz100@gmail.com>
2024-08-11 00:18:13 +0500

    chore: add generators

---
+++ generator/generator.go
@@ -0,0 +1,3 @@
+package generator
+
+//go:generate npx tailwindcss -i ../dist/main.css -o ../dist/tailwind.css && go generate ../views && go run ../cmd/.
--- views/components/selectLBStrat_templ.go
+++ views/components/selectLBStrat_templ.go
@@ -43,7 +43,7 @@ func SelectLBStrat(data []SelectStrat) templ.Component {
 			var templ_7745c5c3_Var2 string
 			templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(strat.Id)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/selectLBStrat.templ`, Line: 12, Col: 24}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/selectLBStrat.templ`, Line: 12, Col: 24}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 			if templ_7745c5c3_Err != nil {
@@ -56,7 +56,7 @@ func SelectLBStrat(data []SelectStrat) templ.Component {
 			var templ_7745c5c3_Var3 string
 			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(strat.Name)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/selectLBStrat.templ`, Line: 12, Col: 39}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/selectLBStrat.templ`, Line: 12, Col: 39}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 			if templ_7745c5c3_Err != nil {
@@ -69,7 +69,7 @@ func SelectLBStrat(data []SelectStrat) templ.Component {
 			var templ_7745c5c3_Var4 string
 			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(strat.Id)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/selectLBStrat.templ`, Line: 12, Col: 94}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/selectLBStrat.templ`, Line: 12, Col: 94}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 			if templ_7745c5c3_Err != nil {
