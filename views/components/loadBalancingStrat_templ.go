cb70a74049f327cf6d672b053e83ff08d01490a1
MuhammadSaad0 <muhammadsaadz100@gmail.com>
2024-08-11 00:22:25 +0500

    fix: remove generators due to directory issues

--- views/components/loadBalancingStrat_templ.go
+++ views/components/loadBalancingStrat_templ.go
@@ -37,7 +37,7 @@ func LoadBalancingStrat(data LoadBalancingData) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(data.Name)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/loadBalancingStrat.templ`, Line: 9, Col: 55}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/loadBalancingStrat.templ`, Line: 9, Col: 55}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
